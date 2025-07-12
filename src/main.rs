c1c74559eb85bd15619dd73bca4b3cbb1a550f4c
wuihee <wuihee@gmail.com>
2025-07-12 17:00:51 +0800

    Updated metadata files.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,1 @@
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ProgramInfo {
-    pub url: String,
-    pub version: Option<String>,
-    pub setup: Option<Vec<String>>,
-    pub build: Vec<String>,
-    pub test: Vec<String>,
-    pub dependencies: Vec<String>,
-    pub source: String,
-    pub executable: String,
-}
-
 fn main() {}
