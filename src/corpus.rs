d8198876f4d187ea40cc01c8e2c5efe5484c453a
wuihee <wuihee@gmail.com>
2025-07-24 16:33:45 +0800

    Update corpus::clone to clone repos into /programs/temp.

--- src/corpus.rs
+++ src/corpus.rs
@@ -1,11 +1,15 @@
 use super::parser::schema::ProgramPair;
+use crate::paths::TEMP_PROGRAMS_DIR;
 use git2::Repository;
 
 // Clone repos in a program pair into a temporary directory.
 pub fn clone(pair: &ProgramPair) {
+    let program_name = &pair.program_name;
     let rust_repository_url = &pair.rust_program.repository_url;
-    println!("Rust Repository URL: {rust_repository_url}");
-    match Repository::clone(&rust_repository_url, "./diffutils") {
+    match Repository::clone(
+        &rust_repository_url,
+        format!("{TEMP_PROGRAMS_DIR}/{program_name}"),
+    ) {
         Ok(_) => println!("Successfully cloned!"),
         Err(error) => println!("Failed to clone: {}", error.message()),
     };
--- src/paths.rs
+++ src/paths.rs
@@ -1,1 +1,2 @@
 pub const METADATA_SCHEMA_FILE: &str = "./metadata/metadata.schema.json";
+pub const TEMP_PROGRAMS_DIR: &str = "./programs/temp";
