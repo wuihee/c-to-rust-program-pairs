b07e78afedcfeafdfc360c3f8a5cd179f4c8b8fb
wuihee <wuihee@gmail.com>
2025-07-26 17:32:54 +0800

    Refactor out duplicate code in download_program_pair

--- src/corpus.rs
+++ src/corpus.rs
@@ -1,49 +1,55 @@
-use super::parser::schema::ProgramPair;
-use super::paths::PROGRAMS_DIRECTORY;
+use super::{parser::schema::ProgramPair, paths::PROGRAMS_DIRECTORY};
 use git2::Repository;
 use std::{error::Error, fs, path::Path};
-use tempfile::tempdir;
+use tempfile;
 
-// TODO: Refactor out redundancy.
-// Clone repos in a program pair into a temporary directory.
+// Downloads a program-pair into the ./programs directory.
+// The C source files will be downloaded to ./programs/<program_name>/c-program.
+// The Rust source files will be downloaded to ./programs/<program_name>/rust-program.
 pub fn download_program_pair(pair: &ProgramPair) -> Result<(), Box<dyn Error>> {
-    // Clone repository into temporary directory.
     let program_name = &pair.program_name;
-    let c_repository_url = &pair.c_program.repository_url;
-    let rust_repository_url = &pair.rust_program.repository_url;
-    let c_temporary_directory = tempdir()?;
-    let rust_temporary_directory = tempdir()?;
-    let c_repository = Repository::clone(
-        &c_repository_url,
-        c_temporary_directory.path().join(&program_name),
+    let base_program_path = Path::new(PROGRAMS_DIRECTORY).join(program_name);
+    let c_program_path = base_program_path.join("c-program");
+    let rust_program_path = base_program_path.join("rust-program");
+    fs::create_dir_all(&c_program_path)?;
+    fs::create_dir_all(&rust_program_path)?;
+
+    download_files(
+        program_name,
+        &c_program_path,
+        &pair.c_program.repository_url,
+        &pair.c_program.source_paths,
     )?;
-    let rust_repository = Repository::clone(
-        &rust_repository_url,
-        rust_temporary_directory.path().join(&program_name),
+    download_files(
+        program_name,
+        &rust_program_path,
+        &pair.rust_program.repository_url,
+        &pair.rust_program.source_paths,
     )?;
 
-    // Create directory for program pair.
-    let program_directory = format!("{PROGRAMS_DIRECTORY}/{program_name}");
-    let rust_program_directory = format!("{program_directory}/rust_program");
-    let c_program_directory = format!("{program_directory}/c_program");
-    fs::create_dir_all(&program_directory)?;
-    fs::create_dir_all(&rust_program_directory)?;
-    fs::create_dir_all(&c_program_directory)?;
+    Ok(())
+}
 
-    // Copy paths from repository into program directory.
-    for file_path in pair.c_program.source_paths.iter() {
-        let file_name = Path::new(file_path).file_name().unwrap();
-        let source = c_repository.workdir().unwrap().join(&file_path);
-        let destination = Path::new(&c_program_directory).join(file_name);
-        if !destination.exists() {
-            fs::copy(source, destination)?;
-        }
-    }
+// TODO: Make this async.
+// Clones a repository copy the relevant files to the program directory.
+fn download_files(
+    program_name: &str,
+    program_directory: &Path,
+    repository_url: &str,
+    files: &Vec<String>,
+) -> Result<(), Box<dyn Error>> {
+    // Clone the repository.
+    let temporary_directory = tempfile::tempdir()?;
+    let repository = Repository::clone(
+        &repository_url,
+        temporary_directory.path().join(&program_name),
+    )?;
 
-    for file_path in pair.rust_program.source_paths.iter() {
+    // Copy the given files from the repository to the given directory.
+    for file_path in files.iter() {
         let file_name = Path::new(file_path).file_name().unwrap();
-        let source = rust_repository.workdir().unwrap().join(&file_path);
-        let destination = Path::new(&rust_program_directory).join(file_name);
+        let source = repository.workdir().unwrap().join(&file_path);
+        let destination = program_directory.join(file_name);
         if !destination.exists() {
             fs::copy(source, destination)?;
         }
